b69e2ba3d10ac81d9263efe0b3e70d381f8a1706
René Ladan <rene0+github@freedom.nl>
2021-10-31 23:15:50 +0100

    Implement uprintln! and measure how long that takes for a given string.
    (cherry picked from commit 56d426b4d32ac69b33481931fd018f64dfcbde72)

--- f3discovery/src/11-usart/src/main.rs
+++ f3discovery/src/11-usart/src/main.rs
@@ -1,17 +1,54 @@
+#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
+use core::fmt::{self, Write};
+
 #[allow(unused_imports)]
-use aux11::{entry, iprint, iprintln};
+use aux11::{entry, iprint, iprintln, usart1};
+
+macro_rules! uprint {
+    ($serial:expr, $($arg:tt)*) => {
+        $serial.write_fmt(format_args!($($arg)*)).ok()
+    };
+}
+
+macro_rules! uprintln {
+    ($serial:expr, $fmt:expr) => {
+        uprint!($serial, concat!($fmt, "\n"))
+    };
+    ($serial:expr, $fmt:expr, $($arg:tt)*) => {
+        uprint!($serial, concat!($fmt, "\n"), $($arg)*)
+    };
+}
+
+struct SerialPort {
+    usart1: &'static mut usart1::RegisterBlock,
+}
+impl fmt::Write for SerialPort {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for c in s.bytes() {
+            while self.usart1.isr.read().txe().bit_is_clear() {}
+            self.usart1.tdr.write(|w| w.tdr().bits(u16::from(c)));
+        }
+        Ok(())
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    let (usart1, _mono_timer, _itm) = aux11::init();
+    let (usart1, mono_timer, mut itm) = aux11::init();
+    let instant = mono_timer.now();
+    let mut serial = SerialPort { usart1 };
+    uprintln!(serial, "The answer is {}", 40 + 2);
 
-    // Send a single character
-    usart1
-        .tdr
-        .write(|w| w.tdr().bits(u16::from(b'X')) );
+    let elapsed = instant.elapsed(); // in ticks
+    iprintln!(
+        &mut itm.stim[0],
+        "`for` loop took {} ticks ({} us)",
+        elapsed,
+        elapsed as f32 / mono_timer.frequency().0 as f32 * 1e6
+    );
 
     loop {}
 }
